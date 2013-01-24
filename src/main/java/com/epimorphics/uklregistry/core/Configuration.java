581d9a4750a3a17d602ffb5c05082fdea0f5a23e
der <dave@epimorphics.com>
2013-01-24 00:01:23 +0000

    Added vocab load at bootstrap time to improve UI render of things like status categories

--- src/main/java/com/epimorphics/uklregistry/core/Configuration.java
+++ src/main/java/com/epimorphics/uklregistry/core/Configuration.java
@@ -23,7 +23,6 @@ import com.epimorphics.uklregistry.store.Description;
 import com.epimorphics.uklregistry.store.Register;
 import com.epimorphics.uklregistry.store.StoreAPI;
 import com.epimorphics.uklregistry.vocab.Registry;
-import com.epimorphics.util.EpiException;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ResIterator;
 import com.hp.hpl.jena.util.FileManager;
@@ -54,15 +53,20 @@ public class Configuration extends ServiceBase implements Service {
         Register root = store.getRegister(getBaseURI() + "/");
         if (root == null) {
             // Blank store, need to install a root register
-            String rootRegisterSrc = getRequiredFileParam(ROOT_REGISTER_FILE_PARAM);
-            Model model = FileManager.get().loadModel(rootRegisterSrc);
-            ResIterator ri = model.listSubjectsWithProperty(RDF.type, Registry.Register);
-            if (!ri.hasNext()) {
-                throw new EpiException("Could not find register resource in the configured default root register");
+            for(String rootRegisterSrc : getRequiredFileParam(ROOT_REGISTER_FILE_PARAM).split("\\|")) {
+                Model model = FileManager.get().loadModel(rootRegisterSrc);
+                ResIterator ri = model.listSubjectsWithProperty(RDF.type, Registry.Register);
+                if (ri.hasNext()) {
+                    Description rootRegister = new Description();
+                    rootRegister.setRoot( ri.next() );
+                    store.storeDescription(rootRegister);
+                } else {
+                    // Simple file such as a vocab
+                    Description vocab = new Description();
+                    vocab.setRoot( model.createResource("file:" + rootRegisterSrc) );
+                    store.storeDescription(vocab);
+                }
             }
-            Description rootRegister = new Description();
-            rootRegister.setRoot( ri.next() );
-            store.storeDescription(rootRegister);
             log.info("Installed bootstrap root register");
         }
 
