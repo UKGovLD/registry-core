38d48be1a837a3703a7f0185dd2657dc63ff6b38
der <dave@epimorphics.com>
2013-05-16 23:18:34 +0100

    Progress on dataset explorer

--- src/main/webapp/ui/js/ds-explore.js
+++ src/main/webapp/ui/js/ds-explore.js
@@ -1,12 +1,61 @@
 /**
  * Data set explorer, based on jsPlumb
  */
+var DATASET_EXPLORER = function() {
+
+    var nodeFor = function(id) {
+        return $(".node[data-id='" + id + "']");
+    };
+    
+    var randomPlacement = function(current, toplace) {
+        var angle = Math.random() * 2 * Math.PI;
+        var yoffset = 180 * Math.sin(angle);
+        var xoffset = 180 * Math.cos(angle);
+        var position = nodeFor(current).offset();
+        position.left = position.left + xoffset;
+        position.top = position.top + yoffset;
+        nodeFor(toplace).offset(position);
+    };
+    
+    var initElement = function() {
+        $(".popinfo").popover();
+        
+        jsPlumb.draggable($(".node"));
+        
+        $(".add-out").click(function(event){
+            var node = $(event.currentTarget).attr('data-node');
+            var linkSet = $(".connection-out[data-out='" + node + "']")
+                            .map( function() { return $(this).attr('data-in'); } )
+                            .get();
+            $.each(linkSet, function(index, linkedDS){
+                var alreadyRequested = false;
+                for (var i = 0; i < index-1; i++) {
+                    if (linkSet[i] === linkedDS) {
+                        alreadyRequested = true;
+                        break;
+                    }
+                }
+                if (!alreadyRequested && nodeFor(linkedDS).length === 0) {
+                    $.get('/ui/dataset-browse-element?uri=' + linkedDS, function(data){
+                        $("#canvas").append(data);
+                        initElement();
+                        randomPlacement(node, linkedDS);
+                    });
+                }
+            });
+        });
+    };
+    
+    // Return the public variables/functions for this module
+    return {
+       initElement : initElement
+    };
+
+}(); // "revealing module" pattern
 
 
 jsPlumb.bind("ready", function() {
-    jsPlumb.draggable($(".node"));
+    DATASET_EXPLORER.initElement();
 });
 
-//$(function() {
-//    $(".node").draggable();
-//});
+
