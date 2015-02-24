9a9babe54704b40877919b0531bee660011fd1f8
sleeper-service <chris.wood@unlikely.name>
2015-02-24 16:11:54 +0000

    Added OpenID Connect discovery

--- src/main/java/com/epimorphics/registry/util/Oauth2Util.java
+++ src/main/java/com/epimorphics/registry/util/Oauth2Util.java
@@ -1,37 +1,9 @@
 package com.epimorphics.registry.util;
-/**
- *       Copyright 2010 Newcastle University
- *
- *          http://research.ncl.ac.uk/smart/
- *
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 
-import com.sun.org.apache.xpath.internal.operations.Bool;
-import org.apache.oltu.oauth2.common.OAuthProviderType;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 import java.io.FileReader;
 import java.util.Properties;
 
-/**
- * Created by lizard on 04/12/2014.
- */
 public class Oauth2Util {
 
 
