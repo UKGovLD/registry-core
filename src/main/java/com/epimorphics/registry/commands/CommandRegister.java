1edf3e9d74a79934171aaddd4730967c61bb81c5
der <dave@epimorphics.com>
2015-04-03 14:57:59 +0100

    Switch representation of account ID since openid is no long the exclusive mechanism (and indeed is essentially deprecated)

--- src/main/java/com/epimorphics/registry/commands/CommandRegister.java
+++ src/main/java/com/epimorphics/registry/commands/CommandRegister.java
@@ -376,7 +376,7 @@ public class CommandRegister extends Command {
             UserInfo userinfo = (UserInfo) SecurityUtils.getSubject().getPrincipal();
             submitter
             .addProperty(FOAF.name, userinfo.getName())
-            .addProperty(FOAF.openid, m.createResource( userinfo.getOpenid()) );
+            .addProperty(FOAF.accountName, m.createResource( userinfo.getOpenid()) );
         } catch (UnavailableSecurityManagerException e) {
             // Occurs during bootstrap
             submitter.addProperty(FOAF.name, "bootstrap");
