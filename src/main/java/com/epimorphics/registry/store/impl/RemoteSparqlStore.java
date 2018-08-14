849448873392efd1f13ee248b910bb165f0e7c5c
Mihajlo Milosavljevic <mihajlo.milosavljevic@epimorphics.com>
2018-08-14 11:44:46 +0200

    Update jena-querybuilder version and finish implementing RemoteSparqlStore

--- src/main/java/com/epimorphics/registry/store/impl/RemoteSparqlStore.java
+++ src/main/java/com/epimorphics/registry/store/impl/RemoteSparqlStore.java
@@ -3,14 +3,17 @@ package com.epimorphics.registry.store.impl;
 import com.epimorphics.appbase.data.impl.RemoteSparqlSource;
 import com.epimorphics.registry.store.Storex;
 import org.apache.jena.arq.querybuilder.SelectBuilder;
+import org.apache.jena.arq.querybuilder.UpdateBuilder;
+import org.apache.jena.arq.querybuilder.handlers.WhereHandler;
 import org.apache.jena.graph.Node;
 import org.apache.jena.graph.NodeFactory;
 import org.apache.jena.graph.Triple;
 import org.apache.jena.query.ResultSet;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.sparql.core.Quad;
+import org.apache.jena.sparql.core.TriplePath;
 
-public class RemoteSparqlStore implements Storex {
+public class RemoteSparqlStore implements Storex, Storex.ReadTransaction, Storex.WriteTransaction {
 
     private RemoteSparqlSource source = new RemoteSparqlSource();
 
@@ -45,8 +48,55 @@ public class RemoteSparqlStore implements Storex {
 
     @Override
     public void write(WriteOperation operation) {
-
+        operation.execute(this);
     }
+
+    @Override
+    public <T> T read(ReadOperation<T> operation) {
+        return operation.execute(this);
+    }
+
+    @Override
+    public void updateGraph(String name, Model graph) {
+        deleteGraph(name);
+        insertGraph(name, graph);
+    }
+
+    @Override
+    public void insertTriple(Triple t) {
+        UpdateBuilder builder = new UpdateBuilder();
+        builder.addInsert(t);
+        source.update(builder.buildRequest());
+    }
+
+    @Override
+    public void insertQuad(Quad t) {
+        UpdateBuilder builder = new UpdateBuilder();
+        builder.addInsert(t);
+        source.update(builder.buildRequest());
+    }
+
+    @Override
+    public void insertGraph(String name, Model graph) {
+        UpdateBuilder builder = new UpdateBuilder();
+        builder.with(name);
+        graph.listStatements().forEachRemaining(stm -> builder.addInsert(stm.asTriple()));
+        source.update(builder.buildRequest());
+    }
+
+    @Override
+    public void deleteGraph(String name) {
+        UpdateBuilder builder = new UpdateBuilder();
+        Node graph = NodeFactory.createURI(name);
+        WhereHandler subQuery = new WhereHandler();
+        Node subject = NodeFactory.createVariable("s");
+        Node predicate = NodeFactory.createVariable("p");
+        Node objekt = NodeFactory.createVariable("o");
+        subQuery.addWhere(new TriplePath( new Triple(subject, predicate, objekt)));
+        builder.addGraph(graph, subQuery);
+        source.update(builder.buildRequest());
+    }
+
 
     private SelectBuilder selectAllBuilder() {
         SelectBuilder builder = new SelectBuilder();
@@ -62,32 +112,5 @@ public class RemoteSparqlStore implements Storex {
         builder.addWhere(new Triple(subject, predicate, objekt));
 
         return builder;
-    }
-
-    private class SparqlTransaction implements Transaction {
-        @Override
-        public void deleteGraph(String name) {
-
-        }
-
-        @Override
-        public void insertGraph(String name, Model graph) {
-
-        }
-
-        @Override
-        public void insertQuad(Quad t) {
-
-        }
-
-        @Override
-        public void insertTriple(Triple t) {
-
-        }
-
-        @Override
-        public void updateGraph(String name, Model graph) {
-            
-        }
     }
 }
