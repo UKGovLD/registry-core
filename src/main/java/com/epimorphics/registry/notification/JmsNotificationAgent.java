204d064d631680cb0d9e75a577480dbf6ed5afcf
Simon Oakes <simon.oakes@epimorphics.com>
2020-02-17 16:05:43 +0000

    REFACTOR Notification agent to use topic parameter

--- src/main/java/com/epimorphics/registry/notification/JmsNotificationAgent.java
+++ src/main/java/com/epimorphics/registry/notification/JmsNotificationAgent.java
@@ -9,22 +9,17 @@ public class JmsNotificationAgent implements NotificationAgent {
     private final Logger log = LoggerFactory.getLogger(JmsNotificationAgent.class);
 
     private TopicConnectionFactory connectionFct;
-    private String destination;
     private Boolean disableMessageId = true;
 
     public void setConnectionFactory(TopicConnectionFactory connectionFct) {
         this.connectionFct = connectionFct;
     }
 
-    public void setDestination(String destination) {
-        this.destination = destination;
-    }
-
     public void setDisableMessageId(Boolean disableMessageId) {
         this.disableMessageId = disableMessageId;
     }
 
-    @Override public void send(String msg, String target, String operation) {
+    @Override public void send(String topic, String msg, String target, String operation) {
         if (connectionFct == null) {
             log.error("Connection factory not configured - unable to send message for target " + target + ".");
         }
@@ -32,17 +27,18 @@ public class JmsNotificationAgent implements NotificationAgent {
         try (TopicConnection connection = connectionFct.createTopicConnection()) {
             connection.start();
             TopicSession session = connection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
-            Topic topic = session.createTopic(destination);
-            TopicPublisher producer = session.createPublisher(topic);
+            Topic topicObj = session.createTopic(topic);
+            TopicPublisher producer = session.createPublisher(topicObj);
             producer.setDisableMessageID(disableMessageId);
 
             Message jmsMsg = session.createTextMessage(msg);
             jmsMsg.setStringProperty("target", target);
             jmsMsg.setStringProperty("operation", operation);
 
+            log.debug("Sending JMS notification to topic: " + topic + " for target: " + target + ", operation: " + operation + ", message: " + msg);
             producer.publish(jmsMsg);
         } catch (Exception e) {
-            log.error("Failed to send STOMP notification for target " + target + ".", e);
+            log.error("Failed to send JMS notification for target: " + target, e);
         }
     }
 }
--- src/main/java/com/epimorphics/registry/notification/NotificationAgent.java
+++ src/main/java/com/epimorphics/registry/notification/NotificationAgent.java
@@ -6,9 +6,10 @@ package com.epimorphics.registry.notification;
 public interface NotificationAgent {
     /**
      * Send a message with the given parameters.
+     * @param topic The topic on which to send the notification.
      * @param msg The current state of the target item, if the operation was constructive. Otherwise, null.
      * @param target The URI of the item which was changed.
      * @param operation The operation that was performed on the registry item.
      */
-    void send(String msg, String target, String operation);
+    void send(String topic, String msg, String target, String operation);
 }
