31b896365bf99e8823bc1831edd97666a75b599e
Simon Oakes <simon.oakes@epimorphics.com>
2020-02-18 11:35:49 +0000

    ADD Doc comments on registry monitor state

--- src/main/java/com/epimorphics/registry/notification/RegistryMonitor.java
+++ src/main/java/com/epimorphics/registry/notification/RegistryMonitor.java
@@ -81,7 +81,15 @@ public class RegistryMonitor implements Startup {
         }
     }
 
+    /**
+     * Defines the state of the registry monitoring configuration.
+     */
     interface State {
+        /**
+         * Determine the topics which should be notified when the item with the given URI changes.
+         * @param targetUri The URI of a register item to check for monitors.
+         * @return The topics to be notified. If empty, the item is not monitored and no notification is required.
+         */
         List<String> getTopics(String targetUri);
     }
 }
