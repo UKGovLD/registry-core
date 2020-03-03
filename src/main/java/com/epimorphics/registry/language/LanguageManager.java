6c270dddc9afae670ebd24d1e1da49601e57f235
Simon Oakes <simon.oakes@epimorphics.com>
2020-03-03 14:19:02 +0000

    ADD Default language and messages accessor on language manager

--- src/main/java/com/epimorphics/registry/language/LanguageManager.java
+++ src/main/java/com/epimorphics/registry/language/LanguageManager.java
@@ -1,5 +1,10 @@
 package com.epimorphics.registry.language;
 
+import com.epimorphics.registry.language.message.FileMessageManager;
+import com.epimorphics.registry.language.message.Messages;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Collections;
 import java.util.List;
 
@@ -24,9 +29,35 @@ public interface LanguageManager {
      */
     Boolean getUseCookies();
 
+    /**
+     * @return The default language in which to render messages and registry content.
+     */
+    String getDefaultLanguage();
+
+    /**
+     * @param lang The two-letter code for the language.
+     * @return The messages for the requested language, if available. Otherwise, a default language.
+     */
+    Messages getMessages(String lang);
+
     class Default implements LanguageManager {
+        private final Logger log = LoggerFactory.getLogger(Default.class);
+        private final FileMessageManager msgManager = new FileMessageManager();
+        private final String defaultLang = "en";
+
         @Override public Boolean isMultilingual() { return false; }
         @Override public List<Language> getLanguages() { return Collections.emptyList(); }
         @Override public Boolean getUseCookies() { return null; }
+        @Override public String getDefaultLanguage() { return defaultLang; }
+
+        @Override public Messages getMessages(String lang) {
+            Messages msgs = msgManager.getMessages(defaultLang);
+            if (msgs != null) {
+                return msgs;
+            }
+
+            log.error("Messages for default language (" + defaultLang + ") are not configured.");
+            return new Messages.Empty();
+        }
     }
 }
--- src/main/java/com/epimorphics/registry/language/MultiLanguageManager.java
+++ src/main/java/com/epimorphics/registry/language/MultiLanguageManager.java
@@ -1,13 +1,34 @@
 package com.epimorphics.registry.language;
 
+import com.epimorphics.registry.language.message.FileMessageManager;
+import com.epimorphics.registry.language.message.Messages;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 
 public class MultiLanguageManager implements LanguageManager {
+    private final Logger log = LoggerFactory.getLogger(MultiLanguageManager.class);
+    private FileMessageManager msgManager = new FileMessageManager();
+
+    private String defaultLang = "en";
     private Boolean useCookies = false;
     private LanguageConfig config;
 
     public void setUseCookies(Boolean useCookies) {
         this.useCookies = useCookies;
+    }
+
+    @Override public Boolean getUseCookies() {
+        return useCookies;
+    }
+
+    public void setDefaultLanguage(String defaultLang) {
+        this.defaultLang = defaultLang;
+    }
+
+    @Override public String getDefaultLanguage() {
+        return defaultLang;
     }
 
     public void setConfig(LanguageConfig config) {
@@ -22,7 +43,18 @@ public class MultiLanguageManager implements LanguageManager {
         return config.getLanguages();
     }
 
-    public Boolean getUseCookies() {
-        return useCookies;
+    @Override public Messages getMessages(String lang) {
+        Messages msgs = msgManager.getMessages(lang);
+        if (msgs != null) {
+            return msgs;
+        }
+
+        msgs = msgManager.getMessages(defaultLang);
+        if (msgs != null) {
+            return msgs;
+        }
+
+        log.error("Messages for language " + lang + " not found, and default language (" + defaultLang + ") messages are not configured.");
+        return new Messages.Empty();
     }
 }
